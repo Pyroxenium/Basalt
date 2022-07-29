a3c2e7a043faacdfbe6c68b53f49aa9be4c2b390
Robert Jelic <36573031+NoryiE@users.noreply.github.com>
2022-07-29 14:32:52 +0200

    Delete oldVersions directory
    Not necessary anymore, if you are looking for different versions go and look into the release section.

--- oldVersions/lastVersion/source/compiler.lua
+++
@@ -1,7 +0,0 @@
-local basaltFileName = "basalt-source.lua"
-local absolutePath = "source"
-local basalt = dofile(fs.combine(absolutePath, "packager.lua")) -- path to packager
-
-local b = fs.open(fs.combine(absolutePath, basaltFileName), "w")
-b.write(basalt)
-b.close()
