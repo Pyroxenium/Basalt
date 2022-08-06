e6717c864856b3093e6f63104d523068957feb83
toastonrye <toastonrye@hotmail.com>
2022-08-06 10:04:22 -0500

    Update basaltPreview.lua
    Copied from the Basalt wiki example

--- examples/basaltPreview.lua
+++ examples/basaltPreview.lua
@@ -1,7 +1,7 @@
 --Basalt configurated installer
 local filePath = "basalt.lua" --here you can change the file path default: basalt.lua
 if not(fs.exists(filePath))then
-    shell.run("pastebin run ESs1mg7P packed true "..filePath) -- this is an alternative to the wget command
+    shell.run("pastebin run ESs1mg7P packed true "..filePath:gsub(".lua", "")) -- this is an alternative to the wget command
 end
 local basalt = require(filePath:gsub(".lua", "")) -- here you can change the variablename in any variablename you want default: basalt
 
