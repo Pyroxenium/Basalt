1c76130086a1a4fd83903e1cf273911da03ff66e
toastonrye <toastonrye@hotmail.com>
2022-08-08 17:09:35 -0500

    Update redstoneAnalogOutput.lua

--- examples/redstoneAnalogOutput.lua
+++ examples/redstoneAnalogOutput.lua
@@ -1,6 +1,7 @@
-local filePath = "basalt.lua" --here you can change the file path default: basalt.lua
+--Basalt configurated installer
+local filePath = "basalt.lua" --here you can change the file path default: basalt
 if not(fs.exists(filePath))then
-    shell.run("pastebin run ESs1mg7P packed true "..filePath) -- this is an alternative to the wget command
+    shell.run("pastebin run ESs1mg7P packed true "..filePath:gsub(".lua", "")) -- this is an alternative to the wget command
 end
 
 -- toastonrye's example: Redstone Analog Output
