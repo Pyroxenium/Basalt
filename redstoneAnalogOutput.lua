6ae0242b003332850a82a491278ea18713227d6e
toastonrye <toastonrye@hotmail.com>
2022-08-08 16:45:39 -0500

    Create redstoneAnalogOutput.lua

---
+++ redstoneAnalogOutput.lua
@@ -0,0 +1,10 @@
+-- Basalt configurated installer
+local filePath = "basalt.lua" -- here you can change the file path default: basalt.lua
+if not(fs.exists(filePath))then
+    shell.run("pastebin run ESs1mg7P packed true "..filePath:gsub(".lua", "")) -- this is an alternative to the wget command
+end
+
+-- toastonrye's example: Redstone Analog Output
+local basalt = require(filePath:gsub(".lua", "")) -- here you can change the variablename in any variablename you want default: basalt
+
+local main = basalt.createFrame()
