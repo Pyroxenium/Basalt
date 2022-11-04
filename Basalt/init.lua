8acb673f74cf4d6f7844d5dbbc9b74ccd8779b5b
Robert Jelic <36573031+NoryiE@users.noreply.github.com>
2022-11-04 21:18:32 +0100

    small hotfix for source project

--- Basalt/init.lua
+++ Basalt/init.lua
@@ -1,9 +1,16 @@
 local curDir = fs.getDir(table.pack(...)[2]) or ""
 
-local defaultPath = package.path
-local format = "%s;/%s/?.lua;/%s/?/init.lua"
-package.path = string.format(format, package.path, curDir,curDir)..string.format(format, package.path, curDir.."/libraries",curDir.."/libraries")..string.format(format, package.path, curDir.."/objects",curDir.."/objects")
+if not(packed)then
+    local defaultPath = package.path
+    local format = "path;/path/?.lua;/path/?/init.lua;"
 
+    local main = format:gsub("path", curDir)
+    local objFolder = format:gsub("path", curDir.."/objects")
+    local libFolder = format:gsub("path", curDir.."/libraries")
+
+
+    package.path = main..objFolder..libFolder..defaultPath
+end
 local Basalt = require("main")
 package.path = defaultPath
 
